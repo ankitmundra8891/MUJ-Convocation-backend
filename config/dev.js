a8b7058ad6a89a4a7253fda8d65f82565c0b010f
Varun Ramnani <varunhhhrahul@gmail.com>
2020-10-14 20:00:34 +0530

    added new branch with correctiosn

--- config/dev.js
+++ config/dev.js
@@ -1,3 +1,13 @@
 module.exports = {
-    mongoURI: "mongodb://localhost:27017/test"
-}
+  mongoURI:
+    'mongodb+srv://manipal:manipal123@manipal-convocation.inwzf.mongodb.net/muj-convocation?retryWrites=true&w=majority',
+  JWT_SECRET: 'qewretrytfgbui',
+  JWT_COOKIE_EXPIRE: 30,
+  NODE_ENV: 'production',
+  SMTP_HOST: 'smtp.gmail.com',
+  SMTP_PORT: '587',
+  SMTP_EMAIL: 'mujconvocation@gmail.com',
+  FROM_EMAIL: 'mujconvocation@gmail.com',
+  FROM_NAME: 'MUJ Convocation',
+  SMTP_PASSWORD: 'utfdklvylvdaufgj',
+};
